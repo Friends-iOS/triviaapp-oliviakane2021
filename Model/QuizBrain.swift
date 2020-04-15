d20d9e7b8033110adaf3162734de0938a106a4a6
Olivia <21kane@friendsseminary.org>
2020-04-15 18:18:25 -0400

    changes 1

--- Model/QuizBrain.swift
+++ Model/QuizBrain.swift
@@ -10,21 +10,53 @@ import Foundation
 
 //begin struct
 
+struct Question{
+    
+    var text : String
+    var answer: String
+    
+    init(q: String, a: String){
+        text = q;
+        answer = a;
+    }
+}
+
 //array of Question structs
+struct QuizBrain{
+    let array_of_questions = [Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:""),Question(q:"", a:"")]
+
+
 
 //variable for our array index
+var arrayIndex : Int
+
 //variable for number of right answers
+var rightAnswers : Int
+
 //variable for number of wrong answers
+var wrongAnswers : Int
+
 //variable to act as a flag if the user reaches the end of the quiz
+var questionCount : Int
 
 //FUNCTION - checking the users answer
     //FORM: func function_name(user's answer (ie a String) -> returns a true or false
-    //conditional statement that will return true if user's answer matches, false if not
+    //conditional statement that will return correct if user's answer matches, wrong if not
+    
+    //send title of the button to the question in the array to see if same answer
+    
+    func checkAnswer(answer:String)  {
+      
+    }
 
 //FUNCTION - retrieving the questions text
     //FORM: func functions_name() -> returns question (ie String)
+    
+    func displayQuestion(Question:String) -> String{
+        return Question
+    }
 
-//FUNCTION - Retrieves user's progress (question number / total number of questions)
+    //FUNCTION - Retrieves user's progress (question number / total number of questions)
     //FORM: func functions_name() -> returns a number (ie Float)
 
 //FUNCTION - retrieves user's score (score right / total number of answers)
@@ -32,3 +64,8 @@ import Foundation
 
 //FUNCTION - updates the variable that is acting as our array index (where in the index are we currently). When the user answers a question, we want to move to the next question
     //FORM: mutating func function_name()
+    mutating func changeQuestion(){
+        
+    }
+
+}
